5bb23182e3abed08dad945f527c6f4ff8d425795
Airisu <erickbarbosabelo@gmail.com>
2026-06-04 15:47:46 -0300

    Create solicita_ajustar.js route file

---
+++ routes/solicita_ajustar.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const Solicitacao = require('../models/Solicitacao'); // model
+
+// Rota para ver o formulário de solicitar (GET) e enviar o pedido (POST)
+// Rota para listar "minhas solicitações"
+// Rotas para aceitar/recusar
+
+module.exports = router;
